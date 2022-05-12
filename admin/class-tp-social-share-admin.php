de80ac4289bffad5f7176713f930499fceaecabd
Tatiane Pires <tatianepiress@gmail.com>
2022-05-12 05:13:51 -0300

    Add empty admin page

--- admin/class-tp-social-share-admin.php
+++ admin/class-tp-social-share-admin.php
@@ -1,5 +1,4 @@
 <?php
-
 class Tp_Social_Share_Admin {
 
 	private $plugin_name;
@@ -14,6 +13,24 @@ class Tp_Social_Share_Admin {
 		$this->plugin_nice_name = $plugin_nice_name;
 		$this->version = $version;
 
+		add_action( 'admin_menu', array( $this, 'add_menu_page' ) );
+	}
+
+	public function add_menu_page() {
+		$args = array(
+			'page_title' => $this->plugin_nice_name,
+			'menu_title' => $this->plugin_nice_name,
+			'capability' => 'manage_options',
+			'menu_slug' => $this->plugin_name,
+			'function' => array( $this, 'admin_page_content' ),
+			'icon_url' => 'dashicons-share',
+			'position' => 75,
+		);
+		add_menu_page( $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'], $args['icon_url'], $args['position']);
+	}
+
+	public function admin_page_content() {
+		echo 'Testing';
 	}
 
 }
